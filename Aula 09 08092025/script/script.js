63fb5af8567a9f907efcc08db339dfb671a97456
Gabriel Moreti <gmoreti87@gmail.com>
2025-09-10 13:52:29 -0300

    aulas web I 08 e 09 de setembro

---
+++ Aula 09 08092025/script/script.js
@@ -0,0 +1,8 @@
+
+console.log("Inicializando Script 01...");
+
+function ler() {
+	console.log("Executando função ler");
+}
+
+console.log("Fechando execução Script 02");
---
+++ Aula 10 090925/script/script.js
@@ -0,0 +1,10 @@
+console.log("Inicializando Script 01")
+
+function ler() {
+    console.log("Executando função ler...")
+
+}
+
+ console.log("Fechando Execução do Script 02")
+
+ 
